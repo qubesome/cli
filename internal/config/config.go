16010ac4dedb809e71cf79b421aee712127c0190
Paulo Gomes <pjbgf@linux.com>
2023-09-28 22:32:18 +0100

    Add basic logging
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,17 @@
 package config
 
 type Config struct {
+	Logging Logging `yaml:"logging"`
+
 	// MimeHandler configures mime types and the specific workloads to handle them.
 	MimeHandlers map[string]MimeHandler `yaml:"mimeHandlers"`
 
 	DefaultMimeHandler *MimeHandler `yaml:"defaultMimeHandler"`
+}
+
+type Logging struct {
+	LogToFile bool   `yaml:"logToFile"`
+	Level     string `yaml:"level"`
 }
 
 type MimeHandler struct {
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -13,12 +14,22 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const (
-	configFile = ".qubesome/qubesome.config"
-)
+func init() {
+	d, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	homedir = d
+}
 
 var (
-	logLevel string
+	homedir string
+)
+
+const (
+	configFile  = ".qubesome/qubesome.config"
+	logFile     = ".qubesome/qubesome.log"
+	logFileMode = 0o600
 )
 
 func main() {
@@ -27,11 +38,29 @@ func main() {
 	flag.StringVar(&in.Name, "name", "",
 		fmt.Sprintf("The name of the workload to be executed. For new workloads use %s import first.", os.Args[0]))
 	flag.StringVar(&in.Profile, "profile", "untrusted", "The profile name which will be used to run the workload.")
-	flag.StringVar(&logLevel, "log-level", "ERROR", "The level of log information to be shown to the user. Options are: ERROR, INFO, DEBUG and WARN.")
 	flag.Parse()
 
-	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slogLevel()})
-	slog.SetDefault(slog.New(h))
+	cfg, err := loadConfig()
+	checkNil(err)
+
+	mw := io.MultiWriter(os.Stdout)
+
+	if cfg.Logging.LogToFile {
+		f, err := os.OpenFile(
+			filepath.Join(homedir, logFile),
+			os.O_RDWR|os.O_CREATE|os.O_APPEND, logFileMode)
+
+		checkNil(err)
+
+		mw = io.MultiWriter(mw, f)
+	}
+
+	slog.SetDefault(slog.New(
+		slog.NewTextHandler(mw,
+			&slog.HandlerOptions{Level: slogLevel(cfg.Logging.Level)}),
+	))
+
+	slog.Debug("qubesome called", "args", os.Args, "config", cfg)
 
 	args := flag.CommandLine.Args()
 	if len(args) == 0 {
@@ -39,11 +68,6 @@ func main() {
 	}
 
 	q := qubesome.New()
-	var err error
-
-	cfg, err := loadConfig()
-	checkNil(err)
-
 	q.Config = cfg
 
 	switch args[0] {
@@ -56,19 +80,17 @@ func main() {
 	checkNil(err)
 }
 
-func slogLevel() slog.Level {
+func slogLevel(logLevel string) slog.Level {
 	switch logLevel {
 	case "ERROR":
 		return slog.LevelError
-	case "INFO":
-		return slog.LevelInfo
 	case "DEBUG":
 		return slog.LevelDebug
-	case "WARN":
-		return slog.LevelWarn
+	case "INFO":
+		fallthrough
+	default:
+		return slog.LevelInfo
 	}
-
-	panic(fmt.Sprintf("unsupported log level: %s", logLevel))
 }
 
 func checkNil(err error) error {
@@ -90,12 +112,7 @@ Supported commands:
 }
 
 func loadConfig() (*config.Config, error) {
-	d, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
-
-	path := filepath.Join(d, configFile)
+	path := filepath.Join(homedir, configFile)
 	cfg := &config.Config{}
 
 	if _, err := os.Stat(path); err != nil && os.IsNotExist(err) {
