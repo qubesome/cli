f2622aab95b994ba3ae531c38f40cf58ab7dcf80
Paulo Gomes <pjbgf@linux.com>
2024-11-18 12:32:46 +0000

    build: Bump golangci-lint to v1.62.0
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- hack/base.mk
+++ hack/base.mk
@@ -1,4 +1,4 @@
-GOLANGCI_VERSION ?= v1.57.2
+GOLANGCI_VERSION ?= v1.62.0
 TOOLS_BIN := $(shell mkdir -p build/tools && realpath build/tools)
 
 GOLANGCI = $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)
