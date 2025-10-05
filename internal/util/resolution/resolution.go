77a04ef347fbb72cae2531b7196fea5e92be70df
Paulo Gomes <pjbgf@linux.com>
2025-10-05 20:12:34 +0100

    Add fallback resolution In cases were xrandr or wlr-randr are not available, or return an error, fall back to a 1440x1080 resolution. This avoids silent failures which can be difficult to pinpoint.
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- internal/util/resolution/resolution.go
+++ internal/util/resolution/resolution.go
@@ -6,18 +6,34 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"log/slog"
 	"strings"
 
 	"github.com/qubesome/cli/internal/files"
 	"golang.org/x/sys/execabs"
 )
 
+const defaultResolution = "1440x1080"
+
 // Primary returns the screen resolution for the primary display.
 func Primary() (string, error) {
-	cmd := execabs.Command(files.XrandrBinary) //nolint
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
+	binaries := []string{files.XrandrBinary, files.WlrRandrBinary}
+	var output []byte
+	var err error
+
+	for _, binary := range binaries {
+		cmd := execabs.Command(binary) //nolint
+		output, err = cmd.Output()
+		if err == nil && len(output) > 0 {
+			break
+		}
+
+		slog.Debug("could not get resolution via %s: %w", binary, err)
+	}
+
+	if len(output) == 0 {
+		slog.Debug("falling back to default resolution", "resolution", defaultResolution)
+		return defaultResolution, nil
 	}
 
 	scanner := bufio.NewScanner(bytes.NewReader(output))
@@ -38,5 +54,5 @@ func Primary() (string, error) {
 		}
 	}
 
-	return "", fmt.Errorf("cannot get resolution")
+	return "", fmt.Errorf("cannot get resolution from output: %q", output)
 }
