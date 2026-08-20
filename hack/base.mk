c3dfaac7cba2fb8e7340e6fad2dad08a3fee773e
Paulo Gomes <paulo@entire.io>
2026-08-20 23:53:11 +0100

    build: Bump golangci-lint to v2.13.0
    Signed-off-by: Paulo Gomes <paulo@entire.io>

--- hack/base.mk
+++ hack/base.mk
@@ -1,5 +1,5 @@
 # renovate: datasource=github-tags depName=golangci/golangci-lint
-GOLANGCI_VERSION ?= v2.12.2
+GOLANGCI_VERSION ?= v2.13.0
 # renovate: datasource=github-tags depName=protocolbuffers/protobuf
 PROTOC_VERSION ?= v35.1
 TOOLS_BIN := $(shell mkdir -p build/tools && realpath build/tools)
